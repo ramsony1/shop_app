fd32d4cb29e3dac13789282bc0ef2430e7c1f962
ramsony1 <62914223+ramsony1@users.noreply.github.com>
2020-08-03 08:22:09 -0400

    Adding the Auth Screen

--- shopApp/lib/main.dart
+++ shopApp/lib/main.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shopApp/screens/product_overview_screen.dart';
+import 'package:shopApp/screens/auth_screen.dart';
 
-import './screens/cart_screen.dart';
-
-import './screens/product_detail_screen.dart';
-import './providers/products.dart';
 import './providers/cart.dart';
 import './providers/orders.dart';
-import './screens/orders_screen.dart';
-import './screens/user_products_screen.dart';
+import './providers/products.dart';
+import './screens/cart_screen.dart';
 import './screens/edit_product_screen.dart';
+import './screens/orders_screen.dart';
+import './screens/product_detail_screen.dart';
+import './screens/user_products_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -36,7 +35,7 @@ class MyApp extends StatelessWidget {
             accentColor: Colors.deepOrange,
             fontFamily: 'Lato',
           ),
-          home: ProductsOverviewScreen(),
+          home: AuthScreen(),
           routes: {
             ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
             CartScreen.routeName: (ctx) => CartScreen(),
